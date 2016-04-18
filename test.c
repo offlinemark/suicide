9b6ee805d61fa279f073903fa2a1fbed81eacc3b
Mark Mossberg <mark.mossberg@gmail.com>
2016-04-18 01:01:22 -0400

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+void dummy(int *x) {}
+
 void func(void)
 {
     int x;
     int y = x + 1;
     puts("harmless function");
+}
+
+void func2(void)
+{
+    int x;
+    int z = x;
+    dummy(&x);
+    int y = x + 1;
 }
 
 int main(int argc, const char **argv)
